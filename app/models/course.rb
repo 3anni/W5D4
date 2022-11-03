ff5525950001cf2c7f8328fd5fcff460ddd6683c
Will Bannister <bannister.will@gmail.com>
2022-11-03 15:56:54 -0700

    completed associations exercise

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,48 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+  validates :name, presence:true
+  validates :prereq_id, presence:false # do we need presence:false here?
+  validates :instructor_id, presence:true
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment,
+    dependent:  :destroy # include any time you have has many and not has many through
+
+  # Referencing its own class
+  # Because of this, we include optional: true
+  belongs_to :prerequisite,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course,
+    optional: true
+
+  belongs_to :instructor,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User
+
+  # Need to put at end. Rails will throw error if you try to run before the association you're using was defined
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
+
+    # has_many :students,
+  #   primary_key: :id,
+  #   foreign_key: :student_id,
+  #   class_name: :Enrollment
+
+
+
 end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,2 +1,34 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :bigint           not null
+#  student_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+  validates :course_id, presence: true
+  validates :student_id, presence: true
+
+  belongs_to :course,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Course
+
+  # any time there's a foreign key, it's a belongs_to
+  belongs_to :student, # arbitrary - can call it anything. call class name by default
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :User
 end
+
+
+
+# Steps
+# 1) foreign keys
+# 2) belongs_to
+# 3) has_manys
+# 4) has_many throughs
+
